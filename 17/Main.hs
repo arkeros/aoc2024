566b00e9fdb72fec39a1e7cae3a05f051b8150f0
arkeros <rafael@arquero.cat>
2024-12-17 13:08:37 +0100

    feat(day17): solve part2 with sbv and z3

--- 17/Main.hs
+++ 17/Main.hs
@@ -3,33 +3,34 @@
 module Main where
 
 import Control.Lens
-import Control.Monad (guard, unless, when)
+import Control.Monad (unless)
 import Control.Monad.State.Strict
 import Data.Array.Unboxed
-import Data.Bits (xor)
-import Data.List (isPrefixOf)
+import Data.Bits (shiftR, xor, (.&.))
+import Data.SBV (SWord64, Symbolic, constrain, minimize, sShiftRight, (./=), (.==))
+import Data.SBV qualified as SBV
 import Data.Void (Void)
-import Debug.Trace (trace)
+import Data.Word (Word64)
 import Text.Megaparsec hiding (State)
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer qualified as L
 
 type Parser = Parsec Void String
-type Output = [Int]
+type Output = [Word64]
 
 data Instruction = ADV | BXL | BST | JNZ | BXC | OUT | BDV | CDV
   deriving (Eq, Show, Ord, Enum, Bounded)
 
-data ProgramState = ProgramState
-  { _registers :: (Int, Int, Int)
-  , _program :: UArray Int Int
+data ProgramState a = ProgramState
+  { _registers :: (a, a, a)
+  , _program :: UArray Int Word64
   , _pc :: Int
   , _output :: Output
   }
-deriving instance Show ProgramState
+
 makeLenses ''ProgramState
 
-registerP :: Parser Int
+registerP :: Parser Word64
 registerP = do
   _ <- string "Register "
   _ <- char 'A' <|> char 'B' <|> char 'C'
@@ -38,13 +39,13 @@ registerP = do
   _ <- newline
   pure r
 
-programP :: Parser (UArray Int Int)
+programP :: Parser (UArray Int Word64)
 programP = do
   _ <- string "Program: "
   xs <- L.decimal `sepBy` char ','
   pure $ listArray (0, length xs - 1) xs
 
-inputP :: Parser ProgramState
+inputP :: Parser (ProgramState Word64)
 inputP = do
   a <- registerP
   b <- registerP
@@ -53,21 +54,21 @@ inputP = do
   program <- programP
   pure $ ProgramState (a, b, c) program 0 []
 
-type AppM = State ProgramState
+type AppM = State (ProgramState Word64)
 
-readLiteralOperand :: AppM Int
+readLiteralOperand :: AppM Word64
 readLiteralOperand = do
   pc' <- use pc
   program' <- use program
-  let literal :: Int = program' ! (pc' + 1)
+  let literal = program' ! (pc' + 1)
   pure $ literal `mod` 8
 
-readComboOperand :: AppM Int
+readComboOperand :: AppM Word64
 readComboOperand = do
   pc' <- use pc
   program' <- use program
   (a, b, c) <- use registers
-  let operand :: Int = program' ! (pc' + 1)
+  let operand :: Word64 = program' ! (pc' + 1)
   case operand of
     4 -> pure a
     5 -> pure b
@@ -79,15 +80,15 @@ readOpcode :: AppM Instruction
 readOpcode = do
   pc' <- use pc
   program' <- use program
-  pure $ toEnum (program' ! pc')
+  pure . toEnum . fromIntegral $ program' ! pc'
 
-registerA :: Lens' ProgramState Int
+registerA :: Lens' (ProgramState a) a
 registerA = registers . _1
 
-registerB :: Lens' ProgramState Int
+registerB :: Lens' (ProgramState a) a
 registerB = registers . _2
 
-registerC :: Lens' ProgramState Int
+registerC :: Lens' (ProgramState a) a
 registerC = registers . _3
 
 executeIntruction :: AppM ()
@@ -111,7 +112,7 @@ executeIntruction = do
       if a /= 0
         then do
           operand <- readLiteralOperand
-          pc .= operand
+          pc .= fromIntegral operand
         else do
           pc += 2
     BXC -> do
@@ -143,33 +144,39 @@ runProgram = do
     executeIntruction
     runProgram
 
-solve1 :: ProgramState -> Output
+solve1 :: ProgramState Word64 -> Output
 solve1 = view output . execState runProgram
 
-isOutputPrefixOfProgram :: AppM Bool
-isOutputPrefixOfProgram = do
-  output' <- use output
-  program' <- use program
-  pure $ output' `isPrefixOf` elems program'
+loop :: SWord64 -> Output -> Symbolic ()
+loop a [] = constrain (a .== 0)
+loop a (x : xs) = do
+  constrain (a ./= 0)
+  b <- pure (a .&. 7)
+  b <- pure (b `xor` 1)
+  c <- pure (a `sShiftRight` b)
+  b <- pure (b `xor` 5)
+  b <- pure (b `xor` c)
+  constrain ((b .&. 7) .== fromIntegral x)
+  a <- pure (a `shiftR` 3)
+  loop a xs
 
-runProgram' :: AppM ()
-runProgram' = do
-  halts <- isHalted
-  unless halts $ do
-    executeIntruction
-    isPrefix <- isOutputPrefixOfProgram
-    when isPrefix runProgram'
+-- 2,4 -> b = a mod 8
+-- 1,1 -> b = b xor 1
+-- 7,5 -> c = a div (2^b)
+-- 1,5 -> b .= b `xor` 5
+-- 4,2 -> b .= b `xor` c
+-- 5,5 -> output (b mod 8)
+-- 0,3 -> a `div` (2 ^ 3)
+-- 3,0 -> jmp if a != 0 to beginning
 
-solve2 :: ProgramState -> Int
-solve2 input = head $ do
-  a <- [198495833 ..]
-  let initialProgram :: ProgramState = input & registerA .~ a
-  -- execute program while the output is a prefix of the program itself
-  let finalProgram = execState runProgram' initialProgram
-  let output' = finalProgram ^. output
-  let program' = finalProgram ^. program
-  guard $ trace (show a <> show output') (output' == elems program')
-  pure a
+solve2 :: ProgramState Word64 -> IO (Maybe Word64)
+solve2 input =
+  do
+    res <- SBV.optLexicographic $ do
+      a <- SBV.free "a" :: Symbolic SWord64
+      minimize "smallest" a
+      loop a (input ^. program . to elems)
+    return $ SBV.getModelValue "a" res
 
 main :: IO ()
 main = do
@@ -177,22 +184,7 @@ main = do
   case parse inputP "stdin" input of
     Left err -> putStrLn $ errorBundlePretty err
     Right x -> do
-      print x
+      -- print x
       print $ solve1 x
-      print $ solve2 x
-
--- 2,4 -> b = a mod 8
--- 1,1 -> b = b xor 1
--- 7,5 -> c = a div (2^b)
--- 1,5 -> a = a `div` (2^b)
--- 4,2 -> b = b `xor` c
--- 5,5 -> output (b mod 8) (cuando es 2?)
--- 0,3 -> a `div` (2 ^ 3)
--- 3,0 -> jmp if a != 0 to beginning
-
--- b = 1..7
--- c = a div (2^(b xor 1))
--- (b xor 1) xor c = ...010
--- => b xor c = (...010 xor 1) = ...011
-
--- (a mod 8) xor (a div (2^(a mod 8))) = ...011
+      a <- solve2 x
+      print a
