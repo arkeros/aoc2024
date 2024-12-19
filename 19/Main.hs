58430f931bf3c53f147d973884e24a2b0088adbb
arkeros <rafael@arquero.cat>
2024-12-19 16:49:58 +0100

    perf(day19): use Trie and dynamic programming

--- 19/Main.hs
+++ 19/Main.hs
@@ -2,10 +2,10 @@
 
 module Main where
 
-import Control.Parallel.Strategies (parMap, rpar, rseq)
-
-import Data.List (isPrefixOf, sort)
-import Data.MemoTrie (memoFix)
+import Data.Array (listArray, (!))
+import Data.Char (ord)
+import Data.IntMap.Strict (IntMap)
+import Data.IntMap.Strict qualified as IntMap
 import Data.Void (Void)
 import Text.Megaparsec hiding (State)
 import Text.Megaparsec.Char
@@ -22,50 +22,63 @@ wordP = many alphaNumChar
 inputP :: Parser Input
 inputP = (,) <$> patternsP <* (newline >> newline) <*> designsP
  where
-  patternsP = (sort) <$> (wordP `sepBy` string ", ")
+  patternsP = (wordP `sepBy` string ", ")
   designsP = wordP `sepBy` newline
 
--- makeStripesP :: [Pattern] -> Parser [String]
--- makeStripesP xs = some patternP <* eof
---  where
---   patterns :: [Parser String] = (string) <$> xs
---   patternP :: Parser String = try (choice patterns)
+data Trie = Node !Bool (IntMap Trie)
+  deriving (Eq, Show)
 
-solve1 :: Input -> Int
-solve1 (patterns, designs) = length . filter (== True) . parMap rseq canBeFormed $ designs
- where
-  canBeFormed :: Design -> Bool
-  canBeFormed = memoFix $ \f str -> case str of
-    [] -> True
-    _ -> any (\prefix -> f (drop (length prefix) str)) candidates
-     where
-      candidates = filter (`isPrefixOf` str) patterns
+insertTrie :: String -> Trie -> Trie
+insertTrie [] (Node _ m) = Node True m
+insertTrie (c : cs) (Node b m) = Node b $ IntMap.insertWith (<>) (ord c) (insertTrie cs mempty) m
+
+lookupTrie :: String -> Trie -> Bool
+lookupTrie [] (Node b _) = b
+lookupTrie (c : cs) (Node _ m) = case IntMap.lookup (ord c) m of
+  Nothing -> False
+  Just t -> lookupTrie cs t
+
+-- Check if a Trie is a prefix of a string
+isPrefixTrie :: String -> Trie -> Bool
+isPrefixTrie [] (Node b _) = b
+isPrefixTrie _ (Node True _) = True
+isPrefixTrie (c : cs) (Node False m) = case IntMap.lookup (ord c) m of
+  Nothing -> False
+  Just t -> isPrefixTrie cs t
+
+-- Given a string, consume the trie and return all possible suffixes
+consumeTrie :: Trie -> String -> [String]
+consumeTrie (Node False _) [] = []
+consumeTrie (Node True _) [] = [""]
+consumeTrie (Node True m) suffix = suffix : consumeTrie (Node False m) suffix
+consumeTrie (Node False m) (c : cs) = case IntMap.lookup (ord c) m of
+  Nothing -> []
+  Just t -> consumeTrie t cs
+
+instance Semigroup Trie where
+  (<>) :: Trie -> Trie -> Trie
+  (Node b m) <> (Node b' m') = Node (b || b') (IntMap.unionWith (<>) m m')
+
+instance Monoid Trie where
+  mempty :: Trie
+  mempty = Node False IntMap.empty
 
 solve :: Input -> (Int, Int)
-solve (patterns, designs) = (,) <$> part1 <*> part2 $ parMap rseq canBeFormed $ designs
+solve (patterns, designs) = (,) <$> part1 <*> part2 $ map nWays $ designs
  where
   part1 = length . filter (> 0)
   part2 = sum
 
-  canBeFormed :: Design -> Int
-  canBeFormed = memoFix $ \f str -> case str of
-    [] -> 1
-    _ -> sum $ map (\prefix -> f (drop (length prefix) str)) candidates
-     where
-      candidates = filter (`isPrefixOf` str) patterns
+  trie = foldr insertTrie mempty patterns
 
--- canBeFormed [] = True
--- canBeFormed str = any (\prefix -> canBeFormed (drop (length prefix) str)) candidates
---  where
---   candidates = filter (`isPrefixOf` str) ps
-
--- solve :: Input -> Int
--- solve (patterns, designs) = length . filter matches $ designs
---  where
---   -- ps = reverse $ sortOn length patterns
---   stripesP = makeStripesP patterns
---   -- matches :: Design -> Bool
---   matches = isRight . parse stripesP "design"
+  -- with dynamic programming, backwards calculate the number of ways to form a pattern
+  -- with the given trie of prefixes
+  nWays :: Design -> Int
+  nWays str = arr ! 0
+   where
+    arr = listArray (0, length str) $ (map f [0 .. length str - 1]) <> [1]
+    f i = sum . map (\j -> arr ! j) . map indexOfSuffix . consumeTrie trie $ drop i str
+    indexOfSuffix suffix = length str - length suffix
 
 main :: IO ()
 main = do
@@ -73,6 +86,7 @@ main = do
   case parse inputP "stdin" input of
     Left err -> putStrLn $ errorBundlePretty err
     Right x -> do
-      print x
-      -- print $ solve1 x
+      -- let trie = foldr insertTrie mempty $ fst x
+      -- print trie
+      -- print $ consumeTrie trie "gbbr"
       print $ solve x
