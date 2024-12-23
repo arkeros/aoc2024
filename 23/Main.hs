6df25704543ebf7a8413ff3c550ec0354303edba
arkeros <rafael@arquero.cat>
2024-12-23 09:28:02 +0100

    feat(day23): solve part2

--- 23/Main.hs
+++ 23/Main.hs
@@ -2,23 +2,21 @@
 
 module Main where
 
-import Control.Lens hiding (children)
-import Data.Array.Unboxed
 import Data.Containers.ListUtils (nubOrd)
-import Data.Graph
-import Data.Maybe (mapMaybe)
+import Data.List (isPrefixOf, sort)
+import Data.MemoTrie (memoFix)
 import Data.Set (Set)
 import Data.Set qualified as Set
-import Data.Tree (flatten, unfoldTree)
 import Data.Void (Void)
+import Debug.Trace (trace)
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer qualified as L
 
 type Parser = Parsec Void String
 type Computer = String
-type Input = [(Computer, Computer)]
-type Key = Computer
+type Input = [Edge]
+type Edge = (Computer, Computer)
 
 inputP :: Parser Input
 inputP = edge `sepBy` newline
@@ -27,43 +25,26 @@ inputP = edge `sepBy` newline
   computer = count 2 alphaNumChar
   edge = (,) <$> computer <* char '-' <*> computer
 
-combinations :: Int -> [a] -> [[a]]
-combinations 0 _ = [[]]
-combinations _ [] = []
-combinations k (x : xs) = ((x :) <$> combinations (k - 1) xs) <> combinations k xs
-
-triples :: [a] -> [(a, a, a)]
-triples = map (\[a, b, c] -> (a, b, c)) . combinations 3
-
-instance Semigroup Bool where
-  (<>) = (&&)
-
--- solve :: Input -> (Int, Int)
--- solve :: [(Computer, Computer)] -> [[Computer]]
--- solve :: Input -> [(Computer, Computer, Computer)]
--- solve :: [(Computer, Computer)] -> Int
-solve input = length . filter (hasT <> isInterconnected) . (>>= triples) . map (map keyFromVertex . flatten) $ components graph
+-- solve2 :: Input -> Int
+solve input = (part1, part2)
  where
-  -- Graph construction
-  (graph, nodeFromVertex, vertexFromKey) =
-    graphFromEdges [(isInteresting key, key, children key) | key <- computers]
-  cellFromVertex = view _1 . nodeFromVertex
-  keyFromVertex = view _2 . nodeFromVertex
-  children :: Computer -> [Computer]
-  children computer = [target | (src, target) <- input, src == computer]
-  computers = nubOrd . (>>= (\(a, b) -> [a, b])) $ input
-  isInteresting :: Computer -> Bool
-  isInteresting ('t' : _) = True
-  isInteresting _ = False
-  edgeSet :: Set (Key, Key)
-  edgeSet = Set.fromList input
-  isInterconnected :: (Computer, Computer, Computer) -> Bool
-  isInterconnected (a, b, c) =
-    ((a, b) `Set.member` edgeSet || (b, a) `Set.member` edgeSet)
-      && ((a, c) `Set.member` edgeSet || (c, a) `Set.member` edgeSet)
-      && ((b, c) `Set.member` edgeSet || (c, b) `Set.member` edgeSet)
-  hasT :: (Computer, Computer, Computer) -> Bool
-  hasT (a, b, c) = any isInteresting [a, b, c]
+  part1 = length . filter (any startsWithT) $ combinations 3
+  -- part2 is the last non-empty list of combinations
+  part2 = last . takeWhile (not . null) $ combinations <$> [2 ..]
+
+  startsWithT = ("t" `isPrefixOf`)
+
+  computers = sort . nubOrd . (>>= (\(a, b) -> [a, b])) $ input
+  edgeSet :: Set Edge
+  edgeSet = Set.fromList . (>>= (\(a, b) -> [(a, b), (b, a)])) $ input
+  combinations :: Int -> [[Computer]]
+  combinations = memoFix $ \f k -> case k of
+    0 -> [[]]
+    1 -> pure <$> computers
+    n -> [x : xs | x <- computers, xs <- f (n - 1), x < head xs, all (isConnected x) xs]
+  isConnected :: Computer -> Computer -> Bool
+  isConnected a b = (a, b) `Set.member` edgeSet
+  {-# INLINE isConnected #-}
 
 main :: IO ()
 main = do
@@ -71,7 +52,4 @@ main = do
   case parse inputP "stdin" input of
     Left err -> putStrLn $ errorBundlePretty err
     Right x -> do
-      -- let trie = foldr insertTrie mempty $ fst x
-      -- print trie
-      -- print $ consumeTrie trie "gbbr"
       print $ solve x
